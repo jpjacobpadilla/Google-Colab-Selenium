6a1a8a729f5c1da694b743888fb57e476d8ff946
Jacob Padilla <jp@jacobpadilla.com>
2023-11-11 13:22:41 -0500

    make sure package is being run on GC

--- google_colab_selenium/__init__.py
+++ google_colab_selenium/__init__.py
@@ -1,9 +1,15 @@
+import os
+
+
+assert 'COLAB_GPU' in os.environ, "This package should only be used in Google Colab Notebooks."
+
+
 def __getattr__(name):
-    if name == 'ChromeDriver':
+    if name == 'ChromeDriver' or name == 'Chrome':
         from google_colab_selenium.chromedriver import ChromeDriver
         return ChromeDriver
 
-    if name == 'UndetectedChromeDriver':
+    if name == 'UndetectedChromeDriver' or name == 'UndetectedChrome':
         from google_colab_selenium.undetected_chromedriver import UndetectedChromeDriver
         return UndetectedChromeDriver
 
