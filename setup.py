f35fa06d70ebc88fd623c177645c75e7de8fc48d
Jacob Padilla <jp@jacobpadilla.com>
2023-11-10 23:53:47 -0500

    initial quick setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Google-Colab-Selenium',
+    description='Easily use Selenium in Google Colab Notebooks!',
+    version='1.0',
+    packages=['google_colab_selenium'],
+    install_requires=['selenium', 'undetected-chromedriver'],
+    author = 'Jacob Padilla',
+    author_email = 'jp@jacobpadilla.com',
+    url='https://github.com/jpjacobpadilla/Google-Colab-Selenium'
+)
