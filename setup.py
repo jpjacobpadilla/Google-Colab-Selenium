fe04e07736c655a9d0fac2e2d68b74b363403578
Jacob Padilla <jp@jacobpadilla.com>
2023-11-11 00:47:17 -0500

    long desc + a few more things

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='Google-Colab-Selenium',
+    name='google-colab-selenium',
     description='Easily use Selenium in Google Colab Notebooks!',
     version='1.0',
     packages=['google_colab_selenium'],
-    install_requires=['selenium', 'undetected-chromedriver'],
+    install_requires=['selenium'],
+    extras_require={
+        'undetected': ['undetected-chromedriver']
+    },
     author = 'Jacob Padilla',
     author_email = 'jp@jacobpadilla.com',
-    url='https://github.com/jpjacobpadilla/Google-Colab-Selenium'
+    url='https://github.com/jpjacobpadilla/Google-Colab-Selenium',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='selenium google-colab webdriver automation chromedriver'
 )
